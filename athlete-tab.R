867a55f4e8be69d3e2af097edf9a36d74186b6aa
Shannon <belfortis18@mail.wlu.edu>
2017-03-20 15:59:36 -0400

    push 3/20

--- athlete-tab.R
+++ athlete-tab.R
@@ -6,42 +6,27 @@ athleteNameUI <- fluidPage(
               unique(allOlympics$Athlete),
               selectize = TRUE
   ),
-  verbatimTextOutput("outName")
+  dataTableOutput('athleteHistory')
+  
 )
 
-athleteNameSrv <- renderPrint(input$athleteName)
+# athleteHandler <- renderPrint(input$athleteName)
+# 
+# 
+# athleteTableUI <- fluidPage(
+#   fluidRow(
+#     allOlympics %>%
+#       filter(Athlete == 'xxx') %>%
+#       #summarise(athleteTable = )
+#     dataTableOutput('table')
+#   )
+# )
+
+#athleteTableSrv <- renderDataTable(allOlympics)
 
 
-#athleteTableUI <- fluidPage(
-#  fluidRow(
-#    allOlympics %>%
-#      filter(Athlete == 'xxx') %>%
-#      #summarise(athleteTable = )
-#    dataTableOutput('table')
-#  )
+#athleteTable <- renderDataTable(
+#  allOlympics %>%
+#    filter(Athlete == outName)
 #)
 
-athleteTableSrv <- renderDataTable(allOlympics)
-
-
-
-<<<<<<< HEAD
-athleteTable <- renderDataTable(
-  allOlympics %>%
-    filter(Athlete == outName)
-)
-
-
-
-<<<<<<< HEAD
-=======
-#  filtered <-
-#    allOlympics %>%
-#    filter(Athlete == formattedName)
-#})
-
-=======
->>>>>>> 2330d40399f4a89981d22b3a81f30feba5acff4f
-
-
->>>>>>> 5d9d9e62fbd4c2c244997fedfe96b7a9d9d35cc1
--- event-tab.R
+++ event-tab.R
@@ -18,3 +18,36 @@ seasonSrv <- renderPrint(input$season)
 #)
 
 #genderSrv <- renderPrint(input$gender)
+
+#EventSrv <- function(input, output) 
+#  output$table <- DT:renderDataTable((DT::datatable{data <- allOlympics
+#  if (input$Season != "All") {
+ #   data <- data[data$Season == inout$season,]
+  #}
+#  if (input&gender !="All" {
+#    data <- data[data$gender == input$gender,]
+#  }
+#    data
+#  )
+#}
+
+EventUI <- fluidPage(
+  titlePanel("xxx"),
+  fluidRow(
+    column(4,
+           selectInput("Season","Season:",
+                       c("All",
+                         unique(as.character(allOlympics$Season)
+                             )))
+  ),
+  column(4,
+         selectInput("Gender",
+                     "Gender:",
+                     c("All",
+                       unique(as.character(allOlympics$Gender)
+                              )))
+  )))
+  
+
+
+
