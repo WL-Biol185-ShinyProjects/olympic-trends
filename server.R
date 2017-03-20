0c0cb199f4a853004e9896a84dd4b273619b4509
Shannon <belfortis18@mail.wlu.edu>
2017-03-20 16:25:14 -0400

    Event tab 3/20

--- server.R
+++ server.R
@@ -9,5 +9,27 @@ function(input, output, session) {
       filter(Athlete == input$athleteName)
     
   })
+  output$eventTable <- DT::renderDataTable(DT::datatable({
+    data <- allOlympics
+    if (input$season != "All") {
+      data <- data[data$Season == input$season,]
+    }
+    if (input$gender != "All") {
+      data <- data[data$Gender == input$gender,]
+    }
+    if (input$year != "All") {
+      data <- data[data$Year == input$year,]
+    }
+    if (input$sport != "All") {
+      data <- data[data$Sport == input$sport,]
+    }
+    if (input$discipline != "All") {
+      data <- data[data$Discipline == input$discipline,]
+    }
+    if (input$event != "All") {
+      data <- data[data$Event == input$event,]
+    }
+    
+  }))
 }
 
