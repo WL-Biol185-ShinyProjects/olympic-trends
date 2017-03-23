c133eb4ee439c6c833f5a4059347a2eaf168541f
Shannon <belfortis18@mail.wlu.edu>
2017-03-23 11:51:26 -0400

    3/23 push

--- event-tab.R
+++ event-tab.R
@@ -1,50 +1,56 @@
 library(shiny)
 library(dplyr)
+library(ggplot2)
 
-eventUI <- fluidPage(
-  titlePanel("Event Winners"),
-  fluidRow(
-    column(4,
-           selectInput("season",
-                       "Season:",
-                       c("All",
-                         unique(as.character(allOlympics$Season))))
-  ),
-  column(4,
-         selectInput("gender",
-                     "Gender:",
-                     c("All",
-                       unique(as.character(allOlympics$Gender))))
-  ),
-  column(4,
-         selectInput("year",
-                     "Year:",
-                     c("All",
-                       unique(as.character(allOlympics$Year))))
-  ),
-  column(4,
-         selectInput("sport",
-                     "Sport:",
-                     c("All",
-                       unique(as.character(allOlympics$Sport))))
-         ),
-  column(4,
-         selectInput("discipline",
-                     "Discipline:",
-                     c("All",
-                       unique(as.character(allOlympics$Discipline))))
-         ),
-  column(4,
-         selectInput("event",
-                     "Event:",
-                     c("All",
-                       unique(as.character(allOlympics$Event))))
-         )
-  ),
-  dataTableOutput('eventTable')
+eventUI <- fluidPage(titlePanel("Event Winners"),
+   fluidRow(
+     column(4,
+            selectizeInput("season",
+                        "Season:",
+                        c("All",
+                          unique(as.character(allOlympics$Season))))
+   ),
+   column(4,
+          selectizeInput("gender",
+                      "Gender:",
+                      c("All",
+                        unique(as.character(allOlympics$Gender))))
+   ),
+   column(4,
+          selectizeInput("year",
+                      "Year:",
+                      c("All",
+                        unique(as.character(allOlympics$Year))))
+   ),
+   column(4,
+          selectizeInput("sport",
+                      "Sport:",
+                      c("All",
+                        unique(as.character(allOlympics$Sport))))
+          ),
+   column(4,
+          selectizeInput("discipline",
+                      "Discipline:",
+                      c("All",
+                        unique(as.character(allOlympics$Discipline))))
+          ),
+   column(4,
+          selectizeInput("event",
+                      "Event:",
+                      c("All",
+                        unique(as.character(allOlympics$Event))))
+          )
+   ),
+   dataTableOutput('eventTable')
+   
+ )
   
-)
-  
-
+# sidebarPanel(
+#   dataTableOutput("Season"),
+#   dataTableOutput("Gender"),
+#   dataTableOutput("Year"),
+#   dataTableOutput("Sport"),
+#   dataTableOutput("Discipline"),
+#   dataTableOutput("Event"))
 
 
